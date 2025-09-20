076d59cd12febdcff03037b21f9c7993187318c0
polyuihplayz1233 <johngirland81@gmail.com>
2025-09-21 00:43:28 +0100

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,6 @@
-import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
-import { Loader } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
 import { cn } from "@/lib/utils";
-
 import { SidebarItem } from "./sidebar-item";
 
 type SidebarProps = {
@@ -22,37 +18,15 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <Link href="/learn">
         <div className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
           <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
-
           <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
             Lingo
           </h1>
         </div>
       </Link>
-
       <div className="flex flex-1 flex-col gap-y-2">
-        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
-        <SidebarItem
-          label="Leaderboard"
-          href="/leaderboard"
-          iconSrc="/leaderboard.svg"
-        />
-        <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
-        <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
-      </div>
-
-      <div className="p-4">
-        <ClerkLoading>
-          <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
-        </ClerkLoading>
-
-        <ClerkLoaded>
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: { userButtonPopoverCard: { pointerEvents: "initial" } },
-            }}
-          />
-        </ClerkLoaded>
+        {/* Add static menu items or mock nav here */}
+        <SidebarItem label="Learn" href="/learn" />
+        <SidebarItem label="Shop" href="/shop" />
       </div>
     </div>
   );
