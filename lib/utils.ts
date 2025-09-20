103b9d824f7e5c417c5f952e6c895dbb587c0991
polyuihplayz1233 <johngirland81@gmail.com>
2025-09-21 00:44:45 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,2 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
-}
+// Database logic removed for local mode.
+export {};
